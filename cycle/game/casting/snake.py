3ff7ef75bd5c88aa310818bf3412fa8875df6467
ksantos16 <u121364866@byui.edu>
2022-03-07 21:55:41 -0500

    dynamic head

--- cycle/game/casting/snake.py
+++ cycle/game/casting/snake.py
@@ -6,16 +6,17 @@ from game.shared.point import Point
 class Snake(Actor):
     """
     A long limbless reptile.
-    
+
     The responsibility of Snake is to move itself.
 
     Attributes:
         _points (int): The number of points the food is worth.
     """
+
     def __init__(self):
         super().__init__()
         self._segments = []
-        self._prepare_body()
+        self._prepare_body(head_symbol="@")
 
     def get_segments(self):
         return self._segments
@@ -34,33 +35,33 @@ class Snake(Actor):
     def get_head(self):
         return self._segments[0]
 
-    def grow_tail(self, number_of_segments):
+    def grow_tail(self, number_of_segments, color):
         for i in range(number_of_segments):
             tail = self._segments[-1]
             velocity = tail.get_velocity()
             offset = velocity.reverse()
             position = tail.get_position().add(offset)
-            
+
             segment = Actor()
             segment.set_position(position)
             segment.set_velocity(velocity)
             segment.set_text("#")
-            segment.set_color(constants.GREEN)
+            segment.set_color(constants.color)
             self._segments.append(segment)
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
-    
-    def _prepare_body(self):
+
+    def _prepare_body(self, head_symbol="@"):
         x = int(constants.MAX_X / 2)
         y = int(constants.MAX_Y / 2)
 
-        for i in range(constants.SNAKE_LENGTH):
+        for i in range(constants.SNAKE_LENGTH, head_symbol="@"):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
-            text = "8" if i == 0 else "#"
+            text = head_symbol if i == 0 else "#"
             color = constants.YELLOW if i == 0 else constants.GREEN
-            
+
             segment = Actor()
             segment.set_position(position)
             segment.set_velocity(velocity)
